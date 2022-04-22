885965fb36fc5307451c80dba76dc46bf5b0b641
Vadym Bei <bey1705@gmail.com>
2022-04-22 12:37:09 +0300

    WebClient was integrated to Algolia Api

--- src/Services/Algolia/Algolia.Core/Infrastructure/Repositories/AlgoliaRepository.cs
+++ src/Services/Algolia/Algolia.Core/Infrastructure/Repositories/AlgoliaRepository.cs
@@ -1,44 +1,37 @@
 ﻿using Algolia.Core.Application.Common.Interfaces;
 using Algolia.Core.Domain.Models;
-using Newtonsoft.Json;
+using Common.Data.WebClient.Extensions;
+using Common.Data.WebClient.Interfaces;
 
 namespace Algolia.Core.Infrastructure.Repositories
 {
     public class AlgoliaRepository : IAlgoliaRepository
     {
+        private readonly IWebClient _webClient;
+
+        public AlgoliaRepository(
+            IWebClient webClient)
+        {
+            _webClient = webClient;
+            _webClient.Configure(c =>
+            {
+                c.WebResourcePath = "algolia-locations-api/algolia-places";
+            });
+        }
+
         public async Task<AlgoliaLocation> SearchLocationByObjectId(string objectId, CancellationToken cancellationToken)
         {
-            var algoliaLocation = new AlgoliaLocation();
-
-            using (var client = new HttpClient())
-            {
-                var response = await client.GetAsync(
-                    $"https://places-dsn.algolia.net/1/places/{objectId}", cancellationToken);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var responceContent = await response.Content.ReadAsStringAsync();
-                    algoliaLocation = JsonConvert.DeserializeObject<AlgoliaLocation>(responceContent);
-                }
-            }
+            var algoliaLocation = await _webClient
+                .Get<AlgoliaLocation>(objectId, cancellationToken);
 
             return algoliaLocation;
         }
 
         public async Task<List<Hit>> SearchLocationsByName(string name, CancellationToken cancellationToken)
         {
-            var algoliaResponce = new AlgoliaResponce();
-            using (var client = new HttpClient())
-            {
-                var response = await client.GetAsync(
-                    $"https://places-dsn.algolia.net/1/places/query?query={name}&type=city&language=ua", cancellationToken);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var responceContent = await response.Content.ReadAsStringAsync();
-                    algoliaResponce = JsonConvert.DeserializeObject<AlgoliaResponce>(responceContent);
-                }
-            }
+            var algoliaResponce = await _webClient
+                .WithQueryParams($"?query={name}&type=city&language=ua")
+                .Get<AlgoliaResponce>("query", cancellationToken);
 
             return algoliaResponce.Hits;
         }
